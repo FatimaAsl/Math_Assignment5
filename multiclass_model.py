eed5d6f6003a80decb8a75c04737da6d4253c5eb
Fatima <Fatemeh.aghapourasl@gmail.com>
2024-01-27 02:10:51 -0500

    Update multiclass_model.py

--- multiclass_model.py
+++ multiclass_model.py
@@ -8,25 +8,17 @@ from tensorflow.keras.layers import Input
 
 def define_dense_model_single_layer(input_length, activation_f='sigmoid', output_length=1):
         model = keras.Sequential([layers.input(shape=(input_length,)),
-                              layers.Dense(output_length,activation=activation_f)
-          ])
+                              layers.Dense(output_length,activation=activation_f)])
     return model
 
 def define_dense_model_with_hidden_layer(input_length, 
                                          activation_func_array=['relu','sigmoid'],
                                          hidden_layer_size=10,
                                          output_length=1):
-    """Define a dense model with a hidden layer with the following parameters:
-    input_length: the number of inputs
-    activation_func_array: the activation function for the hidden layer and the output layer
-    hidden_layer_size: the number of neurons in the hidden layer
-    output_length: the number of outputs (number of neurons in the output layer)"""
-
     model = keras.Sequential([Input(shape=(input_length,)),
                               layers.Dense(hidden_layer_size, activation=activation_func_array[0]),
                               layers.Dense(output_length, activation=activation_func_array[1])
                               ])
-
     return model
 
 
@@ -39,20 +31,11 @@ def get_mnist_data():
     return (x_train, y_train), (x_test, y_test)
 
 def fit_mnist_model(x_train, y_train, model, epochs=2, batch_size=2):
-    """Fit the model to the data.
-    compile the model and add parameters for  the "optimizer", the loss function, 
-    and the metrics, Hint: use categorical crossentropy for the loss function .
-
-    then fit the model on the training data. (pass the epochs and batch_size params)
-    """
     model.compile(optimizer='adam',loss='catagorical_crossentropy', metrics=['accuracy'])
     model.fit9(x_train,y_train,epochs=epochs,batch_size=batch_size)
     return model
   
 def evaluate_mnist_model(x_test, y_test, model):
-    """Evaluate the model on the test data.
-    Hint: use model.evaluate() to evaluate the model on the test data.
-    """
-    loss, accuracy = model.evaluate(x_test,y_test) # model evaluate
+    loss, accuracy = model.evaluate(x_test,y_test)
     return loss, accuracy
 
